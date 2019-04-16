4699a58cef776f1f69a20b10df8b329e1d31861a
Abilio Esteves <abilio.esteves@ibm.com>
2019-04-16 16:44:38 -0300

    fixed usercredentials_test

--- db/usercredentials_test.go
+++ db/usercredentials_test.go
@@ -1,19 +1,8 @@
 package db
 
 import (
-	"time"
-
 	"github.com/google/uuid"
 )
-
-// UserCredential structures the information that defines a user credential
-type UserCredential struct {
-	ID          string    `db:"id"`
-	ClientID    string    `db:"client_id"`
-	Username    string    `db:"username"`
-	Password    string    `db:"password"`
-	DateCreated time.Time `db:"date_created"`
-}
 
 // CreateUserCredential creates a user
 func CreateUserCredential(username, password, clientID string) (string, error) {

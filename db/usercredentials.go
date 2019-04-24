36419b75e0bfdab38a7ea16b47ccdb4b4d38a406
Abilio Esteves <abilio.esteves@ibm.com>
2019-04-24 18:35:31 -0300

    adjusted initialization of the usercredentialsDAO; setup basic authentication logic

--- db/usercredentials.go
+++ db/usercredentials.go
@@ -6,11 +6,11 @@ import (
 	"strings"
 	"time"
 
+	"github.com/sirupsen/logrus"
+
 	"github.com/google/uuid"
 
 	"github.com/labbsr0x/goh/gohtypes"
-
-	"github.com/labbsr0x/whisper/web/config"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -32,6 +32,8 @@ type UserCredentialsDAO interface {
 	CreateUserCredential(username, password, email string) (string, error)
 	UpdateUserCredential(userID string, email, password string) error
 	DeleteUserCredential(userID string) error
+	GetUserCredential(username string) (UserCredential, error)
+	InitFromDatabaseURL(dbURL string) UserCredentialsDAO
 }
 
 // DefaultUserCredentialsDAO a default UserCredentialsDAO interface implementation
@@ -39,24 +41,25 @@ type DefaultUserCredentialsDAO struct {
 	DatabaseURL string
 }
 
-// InitFromWebBuilder initializes a defualt user credentials DAO from web builder
-func (dao *DefaultUserCredentialsDAO) InitFromWebBuilder(builder *config.WebBuilder) *DefaultUserCredentialsDAO {
-	u, err := url.Parse(builder.DatabaseURL)
+// InitFromDatabaseURL initializes a defualt user credentials DAO from web builder
+func (dao *DefaultUserCredentialsDAO) InitFromDatabaseURL(dbURL string) UserCredentialsDAO {
+	u, err := url.Parse(dbURL)
 	gohtypes.PanicIfError("Unable to parse db url", 500, err)
 	dao.DatabaseURL = strings.Replace(u.String(), u.Scheme+"://", "", 1)
 
-	gohtypes.PanicIfError("Not possible to migrate db", 500, dao.Migrate())
+	gohtypes.PanicIfError("Not possible to migrate db", 500, dao.migrate())
 
 	return dao
 }
 
-// Migrate initializes a migration routine to synchronize db and model
-func (dao *DefaultUserCredentialsDAO) Migrate() error {
+// migrate initializes a migration routine to synchronize db and model
+func (dao *DefaultUserCredentialsDAO) migrate() error {
 	db, err := gorm.Open("mysql", dao.DatabaseURL)
 	if err == nil {
+		defer db.Close()
 		db.AutoMigrate(&UserCredential{})
-		db.Close()
 	}
+	logrus.Error(err)
 	return err
 }
 
@@ -64,6 +67,7 @@ func (dao *DefaultUserCredentialsDAO) Migrate() error {
 func (dao *DefaultUserCredentialsDAO) CreateUserCredential(username, password, email string) (string, error) {
 	db, err := gorm.Open("mysql", dao.DatabaseURL)
 	if err == nil {
+		defer db.Close()
 		userCredential := UserCredential{ID: uuid.New().String(), Username: username, Password: password, Email: email, Salt: password}
 		db.NewRecord(userCredential)
 
@@ -74,7 +78,6 @@ func (dao *DefaultUserCredentialsDAO) CreateUserCredential(username, password, e
 		}
 
 		err = fmt.Errorf("Unable to create an user credential: %v", db.GetErrors())
-		db.Close()
 	}
 	return "", err
 }
@@ -87,4 +90,16 @@ func (dao *DefaultUserCredentialsDAO) UpdateUserCredential(userID string, email,
 // DeleteUserCredential deletes the user
 func (dao *DefaultUserCredentialsDAO) DeleteUserCredential(userID string) error {
 	return nil
+}
+
+// GetUserCredential gets an user credential
+func (dao *DefaultUserCredentialsDAO) GetUserCredential(username string) (UserCredential, error) {
+	userCredential := UserCredential{}
+	db, err := gorm.Open("mysql", dao.DatabaseURL)
+	if err == nil {
+		defer db.Close()
+
+		db.Where("username = ?", username).First(&userCredential)
+	}
+	return userCredential, err
 }

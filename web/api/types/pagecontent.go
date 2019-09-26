2247b0a1474da5259d43eeee9d3281658e3c3203
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2019-09-26 12:59:55 -0300

    Implemented interface for generic pages

--- web/api/types/pagecontent.go
+++ web/api/types/pagecontent.go
@@ -2,6 +2,10 @@ package types
 
 import "html/template"
 
+type IPage interface {
+	SetHTML(html template.HTML)
+}
+
 // Page defines the information expected from a page
 type Page struct {
 	HTML template.HTML // a page should have an HTML

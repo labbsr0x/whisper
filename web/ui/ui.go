7e114d0a7d493379ebf75122cdf06c75a9734363
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2019-09-26 12:55:18 -0300

    Create generic page loader

--- web/ui/ui.go
+++ web/ui/ui.go
@@ -1,6 +1,11 @@
 package ui
 
 import (
+	"bytes"
+	"github.com/labbsr0x/goh/gohtypes"
+	"github.com/labbsr0x/whisper/web/api/types"
+	"html/template"
+	"io/ioutil"
 	"net/http"
 	"path"
 )
@@ -9,4 +14,22 @@ import (
 func Handler(baseUIPath string) http.Handler {
 	uiPath := path.Join(baseUIPath, "")
 	return http.FileServer(http.Dir(uiPath))
+}
+
+// LoadPage is used to load a page with the standard layout
+func LoadPage(baseUIPath string, htmlFile string, page types.IPage, w http.ResponseWriter) {
+	buf := new(bytes.Buffer)
+	content := template.Must(template.ParseFiles(path.Join(baseUIPath, htmlFile)))
+
+	err := content.Execute(buf, page)
+	gohtypes.PanicIfError("Unable to load page", http.StatusInternalServerError, err)
+
+	html, err := ioutil.ReadAll(buf)
+	gohtypes.PanicIfError("Unable to read page", http.StatusInternalServerError, err)
+
+	page.SetHTML(template.HTML(html))
+
+	layout := template.Must(template.ParseFiles(path.Join(baseUIPath, "index.html")))
+	err = layout.Execute(w, page)
+	gohtypes.PanicIfError("Unable to load layout", http.StatusInternalServerError, err)
 }

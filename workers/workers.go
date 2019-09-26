a45c2571eb773b4eae8d8a021d65f49b4793d580
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2019-09-26 13:00:26 -0300

    Added workers

--- workers/workers.go
+++ workers/workers.go
@@ -4,13 +4,14 @@ import (
 	"github.com/labbsr0x/whisper/web/config"
 )
 
-type Workers struct {
-	*config.WebBuilder
-	Mail     MailWorkerAPI
+var Mail DefaultMailWorkerAPI
+
+// InitFromWebBuilder initializes the workers
+func InitFromWebBuilder (builder *config.WebBuilder) {
+	Mail.InitFromWebBuilder(builder)
 }
 
-func (w *Workers) InitFromWebBuilder (builder *config.WebBuilder) *Workers {
-	w.Mail = new(DefaultMailWorkerAPI).InitFromWebBuilder(builder)
-
-	return w
+// Run start all the workers
+func Run () {
+	Mail.Start()
 }

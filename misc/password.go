de586b6b9e207a35269da63b57a50102d2c78e6c
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2019-10-07 10:10:28 -0300

    Adding password validation logic to server

--- misc/password.go
+++ misc/password.go
@@ -1,1 +1,40 @@
 package misc
+
+import (
+	"fmt"
+	"strings"
+)
+
+func ValidatePassword (password, username, email string) error {
+	const (
+		minCharacters = 12
+		maxCharacters = 30
+		minUnique = 7
+	)
+
+	if len(password) < minCharacters {
+		return fmt.Errorf("your password should have at least %v characters", minCharacters)
+	}
+
+	if len(password) > maxCharacters {
+		return fmt.Errorf("your password should have at most %v characters", maxCharacters)
+	}
+
+	pass := strings.ToLower(password)
+	user := strings.ToLower(username)
+	mail := strings.ToLower(email)
+
+	if strings.Contains(pass, user) || strings.Contains(user, pass) {
+		return fmt.Errorf("your password is too similar to your username")
+	}
+
+	if strings.Contains(pass, mail) || strings.Contains(mail, pass) {
+		return fmt.Errorf("your password is too similar to your email")
+	}
+
+	if CountUniqueCharacters(pass) < minUnique {
+		return fmt.Errorf("your password should have at least %v unique characters", minUnique)
+	}
+
+	return nil
+}

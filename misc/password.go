f879f9a70ccecd229dc5377b5ed6c41933acff28
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2019-10-10 11:33:27 -0300

    Exposing password rules

--- misc/password.go
+++ misc/password.go
@@ -8,18 +8,18 @@ import (
 )
 
 const (
-	passMinCharacters       = 12
-	passMaxCharacters       = 30
-	passMinUniqueCharacters = 7
+	PasswordMinCharacters       = 12
+	PasswordMaxCharacters       = 30
+	PasswordMinUniqueCharacters = 7
 )
 
-func ValidatePassword (password, username, email string) {
-	if len(password) < passMinCharacters {
-		gohtypes.Panic(fmt.Sprintf("Your password should have at least %v characters", passMinCharacters), http.StatusBadRequest)
+func ValidatePassword(password, username, email string) {
+	if len(password) < PasswordMinCharacters {
+		gohtypes.Panic(fmt.Sprintf("Your password should have at least %v characters", PasswordMinCharacters), http.StatusBadRequest)
 	}
 
-	if len(password) > passMaxCharacters {
-		gohtypes.Panic(fmt.Sprintf("Your password should have at most %v characters", passMaxCharacters), http.StatusBadRequest)
+	if len(password) > PasswordMaxCharacters {
+		gohtypes.Panic(fmt.Sprintf("Your password should have at most %v characters", PasswordMaxCharacters), http.StatusBadRequest)
 	}
 
 	pass := strings.ToLower(password)
@@ -34,11 +34,11 @@ func ValidatePassword (password, username, email string) {
 		gohtypes.Panic("Your password is too similar to your email", http.StatusBadRequest)
 	}
 
-	if CountUniqueCharacters(pass) < passMinUniqueCharacters {
-		gohtypes.Panic(fmt.Sprintf("your password should have at least %v unique characters", passMinUniqueCharacters), http.StatusBadRequest)
+	if CountUniqueCharacters(pass) < PasswordMinUniqueCharacters {
+		gohtypes.Panic(fmt.Sprintf("your password should have at least %v unique characters", PasswordMinUniqueCharacters), http.StatusBadRequest)
 	}
 }
 
-func GetPasswordTooltip () string {
-	return fmt.Sprintf("<div style=\"text-align: left;\"> Password Rules:<br> 1. At least %v characters<br> 2. At most %v characters<br> 3. At least %v unique characters<br> 4. Differ from username and email<br> </div>", passMinCharacters, passMaxCharacters, passMinUniqueCharacters)
+func GetPasswordTooltip() string {
+	return fmt.Sprintf("<div style=\"text-align: left;\"> Password Rules:<br> 1. At least %v characters<br> 2. At most %v characters<br> 3. At least %v unique characters<br> 4. Differ from username and email<br> </div>", PasswordMinCharacters, PasswordMaxCharacters, PasswordMinUniqueCharacters)
 }

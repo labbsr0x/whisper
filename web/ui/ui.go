7d5dc9b6d0c367db166439285f6a0bd80b69dd5f
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2019-11-07 12:14:16 -0300

    Fixing front-end

--- web/ui/ui.go
+++ web/ui/ui.go
@@ -3,6 +3,7 @@ package ui
 import (
 	"bytes"
 	"github.com/labbsr0x/goh/gohtypes"
+	"github.com/labbsr0x/whisper/misc"
 	"html/template"
 	"io/ioutil"
 	"net/http"
@@ -11,17 +12,15 @@ import (
 
 // Enum
 const (
-	Consent           = "consent.html"
-	EmailConfirmation = "email_confirmation.html"
-	Layout            = "index.html"
-	Login             = "login.html"
-	Registration      = "registration.html"
-	Update            = "update.html"
+	ChangePasswordStep1 = "change_password_step_1.html"
+	ChangePasswordStep2 = "change_password_step_2.html"
+	Consent             = "consent.html"
+	EmailConfirmation   = "email_confirmation.html"
+	Layout              = "index.html"
+	Login               = "login.html"
+	Registration        = "registration.html"
+	Update              = "update.html"
 )
-
-type IPage interface {
-	SetHTML(html template.HTML)
-}
 
 // Handler defines the handler for ui requests
 func Handler(baseUIPath string) http.Handler {
@@ -30,7 +29,11 @@ func Handler(baseUIPath string) http.Handler {
 }
 
 // Render render a page in the response
-func WritePage(w http.ResponseWriter, baseUIPath, htmlFile string, page IPage) {
+func WritePage(w http.ResponseWriter, baseUIPath, htmlFile string, page misc.IPage) {
+	if page == nil {
+		page = &misc.BasePage{}
+	}
+
 	buf := new(bytes.Buffer)
 	content := template.Must(template.ParseFiles(path.Join(baseUIPath, htmlFile)))
 

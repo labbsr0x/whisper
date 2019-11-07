9501c6019a47c653da59f51fd6af5c8e20e8cb9e
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2019-11-07 12:14:43 -0300

    Updating types to the new way

--- web/api/types/consent.go
+++ web/api/types/consent.go
@@ -1,19 +1,14 @@
 package types
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/labbsr0x/goh/gohtypes"
 	"github.com/labbsr0x/whisper/misc"
-	"github.com/sirupsen/logrus"
 	"html/template"
-	"io/ioutil"
-	"net/http"
 )
 
 // ConsentPage defines the data needed to build a consent page
 type ConsentPage struct {
-	Page
+	misc.BasePage
 	ClientURI       string
 	ClientName      string
 	RequestedScopes []misc.GrantScope
@@ -31,22 +26,9 @@ type ConsentRequestPayload struct {
 	Remember   bool
 }
 
-// InitFromRequest initializes the consent payload from an http request
-func (payload *ConsentRequestPayload) InitFromRequest(r *http.Request) *ConsentRequestPayload {
-	data, err := ioutil.ReadAll(r.Body)
-	gohtypes.PanicIfError("Not possible to parse registration payload", http.StatusBadRequest, err)
-
-	err = json.Unmarshal(data, &payload)
-	logrus.Debugf("Payload: '%v' Error: %v", payload, err)
-
+func (payload *ConsentRequestPayload) Check() error {
 	payload.Remember = true
-	payload.check()
 
-	return payload
-}
-
-// check verifies if the consent payload is ok
-func (payload *ConsentRequestPayload) check() error {
 	if len(payload.Challenge) == 0 {
 		return fmt.Errorf("Incomplete form data")
 	}

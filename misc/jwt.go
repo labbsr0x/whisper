d5a2803f4086392400759ab5560e1b4bb57673d7
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2019-11-07 12:13:49 -0300

    Updating misc

--- misc/jwt.go
+++ misc/jwt.go
@@ -16,8 +16,7 @@ func GenerateToken(secret string, data jwt.MapClaims) (string, error) {
 	return token.SignedString([]byte(secret))
 }
 
-// ExtractClaimsTokenFromRequest extract a jwt token from a given request
-func ExtractClaimsTokenFromRequest(secret string, r *http.Request) jwt.MapClaims {
+func ParseToken(tokenString, secret string) (jwt.MapClaims, error) {
 	keyFunc := func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
@@ -26,27 +25,36 @@ func ExtractClaimsTokenFromRequest(secret string, r *http.Request) jwt.MapClaims
 		return []byte(secret), nil
 	}
 
-	tokenString, err := url.QueryUnescape(r.URL.Query().Get("token"))
-	gohtypes.PanicIfError("Unable to retrieve the email confirmation token", http.StatusBadRequest, err)
-
 	token, err := jwt.Parse(tokenString, keyFunc)
-	gohtypes.PanicIfError("Unable to parse the email confirmation token", http.StatusBadRequest, err)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to parse the email confirmation token")
+	}
 
 	if !token.Valid {
-		gohtypes.Panic("Token is not valid", http.StatusBadRequest)
+		return nil, fmt.Errorf("Token is not valid")
 	}
 
 	claims, ok := token.Claims.(jwt.MapClaims)
 	if !ok {
-		gohtypes.Panic("Unable to parse claims from the email confirmation token", http.StatusBadRequest)
+		return nil, fmt.Errorf("Unable to parse claims from the email confirmation token")
 	}
 
 	exp, ok := claims["exp"].(int64)
 	if ok && exp > time.Now().Unix() {
-		gohtypes.Panic("Expired token", http.StatusBadRequest)
+		return nil, fmt.Errorf("Expired token")
 	}
 
-	return claims
+	return claims, nil
+}
+
+// ExtractClaimsTokenFromRequest extract a jwt token from a given request
+func ExtractClaimsTokenFromRequest(secret string, r *http.Request) (jwt.MapClaims, error) {
+	tokenString, err := url.QueryUnescape(r.URL.Query().Get("token"))
+	if err != nil {
+		return nil, fmt.Errorf("Unable to retrieve the email confirmation token")
+	}
+
+	return ParseToken(tokenString, secret)
 }
 
 // UnmarshalEmailConfirmationToken verify it is an email confirmation token and extract the main confirmation
@@ -70,23 +78,23 @@ func UnmarshalEmailConfirmationToken(claims jwt.MapClaims) (username, challenge 
 }
 
 // UnmarshalChangePasswordToken verify it is an change password token and extract the main confirmation
-func UnmarshalChangePasswordToken(claims jwt.MapClaims) (username, challenge string) {
+func UnmarshalChangePasswordToken(claims jwt.MapClaims) (string, string, error) {
 	cp, ok := claims["cp"].(bool)
 	if !ok || !cp {
-		gohtypes.Panic("Change password token not valid", http.StatusNotAcceptable)
+		return "", "", fmt.Errorf("Change password token not valid")
 	}
 
-	username, ok = claims["sub"].(string)
+	username, ok := claims["sub"].(string)
 	if !ok {
-		gohtypes.Panic("Unable to find the user", http.StatusNotFound)
+		return "", "", fmt.Errorf("Unable to find the user")
 	}
 
-	challenge, ok = claims["challenge"].(string)
+	redirectTo, ok := claims["redirect_to"].(string)
 	if !ok {
-		gohtypes.Panic("Unable to find the login challenge", http.StatusNotFound)
+		return "", "", fmt.Errorf("Unable to find the user")
 	}
 
-	return
+	return username, redirectTo, nil
 }
 
 func GetEmailConfirmationToken(secret, username, challenge string) string {
@@ -104,13 +112,17 @@ func GetEmailConfirmationToken(secret, username, challenge string) string {
 	return token
 }
 
-func GetChangePasswordToken(secret, username, challenge string) string {
+func GetChangePasswordToken(secret, username, redirectTo string) string {
+	if len(redirectTo) == 0 {
+		redirectTo = "/login"
+	}
+
 	claims := jwt.MapClaims{
-		"sub":       username,                                // Subject
-		"exp":       time.Now().Add(10 * time.Minute).Unix(), // Expiration
-		"challenge": challenge,                               // Login Challenge
-		"cp":       true,                                    // Change Password Token
-		"iat":       time.Now().Unix(),                       // Issued At
+		"sub":         username,                                // Subject
+		"redirect_to": redirectTo,                              // Redirect Back To
+		"exp":         time.Now().Add(10 * time.Minute).Unix(), // Expiration
+		"cp":          true,                                    // Change Password Token
+		"iat":         time.Now().Unix(),                       // Issued At
 	}
 
 	token, err := GenerateToken(secret, claims)
